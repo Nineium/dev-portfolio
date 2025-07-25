ebd1cb3be15bb18d6d4cd4b80e2ec0bc91d1bc50
Nineium <90992498+Nineium@users.noreply.github.com>
2025-07-25 04:39:22 +0200

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="w-full fixed bottom-0 left-0 z-30 bg-white dark:bg-gray-900 text-sm text-gray-600 dark:text-gray-400 py-2 text-center shadow-inner">
+      © 2025 Jan Marszłek. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
